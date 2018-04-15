8daeae0d559dbcd5b5c14d0873e4371aca474ec5
mqr123 <1013670984@qq.com>
2018-04-15 14:01:32 +0800

    优化HTMLCollection集合获取下标问题

--- Tab/Tab.js
+++ Tab/Tab.js
@@ -2,9 +2,8 @@
  * tab切换
  * author:maqianru
  * creactDate:2018/4/12
+ * updateDate:2018/4/15
  */
-
-
 
 class Tab {
 	constructor(options) {
@@ -26,30 +25,16 @@ class Tab {
 		return this
 	}
 	change() {
-		//因获取的是HTMLcollection集合,无法根据元素获取下标，so使用重新格式化数组的形式定义
-		let navArr = [],
-			contentArr = [];
-		for(var i = 0; i < this.options.navDoms.length; i++) {
-			navArr.push({
-				key: i,
-				value: this.options.navDoms[i]
-			})
-		}
-		for(var i = 0; i < this.options.contentDoms.length; i++) {
-			contentArr.push({
-				key: i,
-				value: this.options.contentDoms[i]
-			})
-		}
-		//获取当前事件对象的下标
+		//获取当前事件对象的下标 __this指代当前事件对象
 		for(var i = 0; i < this.options.navDoms.length; i++) {
 			let __this = this.options.navDoms[i];
 			__this.addEventListener(this.options.type, () => {
 				this.classList(this.options.navDoms, __this, "open")
-				let index = navArr.filter((k, index) => {
-					return navArr[index].value == __this ? true : false;
-				})[0].key;
-				this.classList(this.options.contentDoms, contentArr[index].value, "open")
+				let index;
+				for(var k in this.options.navDoms) {
+					if(this.options.navDoms[k] == __this) index = k;
+				}
+				this.classList(this.options.contentDoms, this.options.contentDoms[index], "open")
 			})
 		}
 	}
@@ -64,10 +49,6 @@ class Tab {
 
 }
 
-//问题：HTMLCollection集合获取对象下标
-//解决办法：重新定义数组替代集合对象，暂时没找到更好的办法
-
-
 
 /*=========================================            使用方法：            =========================================*/
 /*
@@ -78,4 +59,4 @@ class Tab {
  * 	contentDoms:contentDoms		//切换区域列表
  * })
  */
- /*==================================================================================================================*/
+/*==================================================================================================================*/
